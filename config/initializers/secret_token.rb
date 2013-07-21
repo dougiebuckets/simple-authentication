662f1db83b6580b8dd1a0baa22e51818863e201c
Doug Crescenzi <doug@telapi.com>
2013-07-21 17:07:23 -0400

    working instance of a bare bones authentication system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleAuthentication::Application.config.secret_key_base = 'f9901ab906a4d1b4f7c9c23e9a299f3c19005b7ff5f4873e315f9ae056c061b9d76a7893b2636206ab3a250986c51ab97bc755442d4909d139a761d6936e48f3'
